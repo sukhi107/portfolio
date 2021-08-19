54a9f83ad7a29f4dbce95c352510ac8750752834
sukhi107 <49531787+sukhi107@users.noreply.github.com>
2021-08-19 19:28:55 +0100

    Add files via upload
    First commit

---
+++ Personal-Portfolio/script.js
@@ -0,0 +1,29 @@
+function display1() {
+    let image = document.getElementById("screenshot-1");
+    let displaySetting = image.style.display;
+    if (displaySetting === 'block') {
+        image.style.display = 'none';
+    } else {
+        image.style.display = 'block';
+    }
+}
+
+function display2() {
+    let image = document.getElementById("screenshot-2");
+    let displaySetting = image.style.display;
+    if (displaySetting === 'block') {
+        image.style.display = 'none';
+    } else {
+        image.style.display = 'block';
+    }
+}
+
+function display3() {
+    let image = document.getElementById("screenshot-3");
+    let displaySetting = image.style.display;
+    if (displaySetting === 'block') {
+        image.style.display = 'none';
+    } else {
+        image.style.display = 'block';
+    }
+}
