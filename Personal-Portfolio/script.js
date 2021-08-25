10f0e78e0b3da3653b4bfb99c5b6bceeb19d235f
sukhi107 <49531787+sukhi107@users.noreply.github.com>
2021-08-25 15:39:18 +0100

    Update script.js
    Added function for time website has been running for and added comments.

--- Personal-Portfolio/script.js
+++ Personal-Portfolio/script.js
@@ -1,3 +1,18 @@
+// Code for second counter.
+const start = (new Date("Aug 25, 2021 14:08:00").getTime()) / 1000;
+const now = Math.floor(Date.now() / 1000);
+let runningTime = now - start;
+let el = document.getElementById("seconds-counter");
+
+function incrementSeconds() {
+    runningTime += 1;
+    el.innerText = runningTime;
+}
+
+var cancel = setInterval(incrementSeconds, 1000);
+
+// Code for displaying projects.
+
 function display1() {
     let image = document.getElementById("screenshot-1");
     let displaySetting = image.style.display;
